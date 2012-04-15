0ac1b824c8ee04f5eb8d3235a85470601ac76aa1
Jay Phelps <jay@jayphelps.com>
2012-04-15 02:02:58 -0700

    added some more keycodes but still incomplete and not normalized

--- shortcut.js
+++ shortcut.js
@@ -33,9 +33,69 @@
         }
     }
 
+    // Incomplete list and not normalized for browser issues...yet.
     var keyCodeTable = {
-        'a': 65,
-        'b': 66
+        'backspace':     8,
+        'tab':           9,
+        'enter':        13,
+        'shift':        16,
+        'ctrl':         27,
+        'alt':          18,
+        'break':        19,
+        'pause':        19,
+        'capslock':     20,
+        'esc':          27,
+        'escape':       27,
+        'pageup':       33,
+        'pagedown':     34,
+        'end':          35,
+        'home':         36,
+        'arrowleft':    37,
+        'arrowup':      38,
+        'arrowright':   39,
+        'arrowdown':    40,
+        'insert':       45,
+        'delete':       46,
+        '0':            48,
+        '1':            49,
+        '2':            50,
+        '3':            51,
+        '4':            52,
+        '5':            53,
+        '6':            54,
+        '7':            55,
+        '8':            56,
+        '9':            57,
+        ':':            59,
+        ';':            59,
+        '+':            61,
+        '=':            61,
+        'a':            65,
+        'b':            66,
+        'c':            67,
+        'd':            68,
+        'e':            69,
+        'f':            70,
+        'g':            71,
+        'h':            72,
+        'i':            73,
+        'j':            74,
+        'k':            75,
+        'l':            76,
+        'm':            77,
+        'n':            78,
+        'o':            79,
+        'p':            80,
+        'q':            81,
+        'r':            82,
+        's':            83,
+        't':            84,
+        'u':            85,
+        'v':            86,
+        'w':            87,
+        'x':            88,
+        'y':            89,
+        'z':            90
     };
 
     function matchModifier(str) {
@@ -84,7 +144,7 @@
         options[modifier] = true;
 
         for (var i = 1, l = keys.length; i < l; i++) {
-            var currKey = keys[i];
+            var currKey = keys[i].toLowerCase();
             modifier = matchModifier(currKey);
 
             if (modifier) {
